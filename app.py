ebfbb7dfa7f3fa87181d0f377509d20173cace46
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-02-15 16:11:04 +0530

    Ui

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+import streamlit as st
+from keras.preprocessing import image
+
+def classify_fun(text):
+    st.header("good")
+
+# UI
+
+st.title("Classifier")
+
+st.subheader("Check whether it is a dog or cat")
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    clicked=st.button("Predict")
+    if clicked:
+        classify_fun(uploaded_file)
