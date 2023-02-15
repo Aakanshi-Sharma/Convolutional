fdd067bd7f54d19300875c1a6e5752202ea9b231
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-02-15 16:46:44 +0530

    application completed

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 import numpy as np
+import pickle
 import streamlit as st
-from keras.preprocessing import image
+import keras.utils as image
+
+model = pickle.load(open("model.pkl", "rb"))
 
 
 def classify_fun(text):
-    st.write(text.name)
-    # st.image(text)
+    test_image = image.load_img(text, target_size=(64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    result = model.predict(test_image)
+    st.title(result)
+    if result[0][0] == 1:
+        st.title("Dog")
+    else:
+        st.title("Cat")
+    # st.image(test_image)
 
 
 # UI
-
 st.title("Classifier")
-
 st.subheader("Check whether it is a dog or cat")
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
+    st.image(uploaded_file, width=300)
     clicked = st.button("Predict")
     if clicked:
         classify_fun(uploaded_file)
