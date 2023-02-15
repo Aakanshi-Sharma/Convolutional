f17603991dc18e77af2df60b29d048710c7ca7f8
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-02-15 16:16:47 +0530

    pickle conversion

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import numpy as np
 import streamlit as st
 from keras.preprocessing import image
 
+
 def classify_fun(text):
-    st.header("good")
+    st.write(text.name)
+    # st.image(text)
+
 
 # UI
 
@@ -13,6 +16,6 @@ st.subheader("Check whether it is a dog or cat")
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    clicked=st.button("Predict")
+    clicked = st.button("Predict")
     if clicked:
         classify_fun(uploaded_file)
